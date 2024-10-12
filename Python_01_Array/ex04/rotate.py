6487bfe633b93779c5b5c87dd89b25374f87998b
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-10-12 20:28:32 +0100

    Array: ex04: rotate me

--- Python_01_Array/ex04/rotate.py
+++ Python_01_Array/ex04/rotate.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def ft_rotate(img: np.ndarray) -> np.ndarray:
+    rotated_img = np.rot90(img)
+    print(f'New shape after Transpose: {rotated_img.shape[:2]}')
+    return rotated_img
+
+
 def ft_zoom(rgb_img: np.ndarray) -> np.ndarray:
     """
         Convert an RGB image to grayscale and zoom in on a region.
@@ -22,7 +28,8 @@ def ft_zoom(rgb_img: np.ndarray) -> np.ndarray:
 
     zoomed_img_shape = zoomed_img.shape
 
-    print(f'New shape after slicing: {zoomed_img_shape} or {zoomed_img_shape[:2]}')
+    print(f'The shape of image is: {zoomed_img_shape} '
+          'or {zoomed_img_shape[:2]}')
 
     return zoomed_img
 
@@ -45,12 +52,13 @@ def main():
     img_path = 'animal.jpeg'
 
     original_img = ft_load(img_path)
-    print(original_img)
-
     zoomed_img = ft_zoom(original_img)
     print(zoomed_img)
 
-    display_img(zoomed_img)
+    rotated_img = ft_rotate(zoomed_img)
+    print(rotated_img)
+
+    display_img(rotated_img)
 
 
 if __name__ == '__main__':
