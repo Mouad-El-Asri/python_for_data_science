60b735a34bf3a8b9cd2d7da50e22c1a66230e4b3
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-11-12 12:35:21 +0100

    Refactor King class methods to include type hints for better clarity

--- Python_03_OOP/ex02/DiamondTrap.py
+++ Python_03_OOP/ex02/DiamondTrap.py
@@ -3,18 +3,18 @@ from S1E7 import Baratheon, Lannister
 
 class King(Baratheon, Lannister):
     """A class representing the King Joffrey Baratheon character."""
-    def get_eyes(self):
+    def get_eyes(self) -> str:
         """A getter for the eyes attribute."""
         return self.eyes
 
-    def set_eyes(self, eyes):
+    def set_eyes(self, eyes) -> None:
         """A setter for the eyes attribute."""
         self.eyes = eyes
 
-    def get_hairs(self):
+    def get_hairs(self) -> str:
         """A getter for the hairs attribute."""
         return self.hairs
 
-    def set_hairs(self, hairs):
+    def set_hairs(self, hairs) -> None:
         """A setter for the hairs attribute."""
         self.hairs = hairs
